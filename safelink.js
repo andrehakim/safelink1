b06f70de3ca776e3dc6fadbeb6916052bd918253
andrehakim <urangkali@gmail.com>
2025-03-25 18:13:35 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -1,28 +1,73 @@
-var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(input){var output="";var chr1,chr2,chr3,enc1,enc2,enc3,enc4;var i=0;input=Base64._utf8_encode(input);while(i<input.length){chr1=input.charCodeAt(i++);chr2=input.charCodeAt(i++);chr3=input.charCodeAt(i++);enc1=chr1>>2;enc2=((chr1&3)<<4)|(chr2>>4);enc3=((chr2&15)<<2)|(chr3>>6);enc4=chr3&63;if(isNaN(chr2)){enc3=enc4=64;}else if(isNaN(chr3)){enc4=64;}
-output=output+ this._keyStr.charAt(enc1)+ this._keyStr.charAt(enc2)+ this._keyStr.charAt(enc3)+ this._keyStr.charAt(enc4);}
-return output;},decode:function(input){var output="";var chr1,chr2,chr3;var enc1,enc2,enc3,enc4;var i=0;input=input.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(i<input.length){enc1=this._keyStr.indexOf(input.charAt(i++));enc2=this._keyStr.indexOf(input.charAt(i++));enc3=this._keyStr.indexOf(input.charAt(i++));enc4=this._keyStr.indexOf(input.charAt(i++));chr1=(enc1<<2)|(enc2>>4);chr2=((enc2&15)<<4)|(enc3>>2);chr3=((enc3&3)<<6)|enc4;output=output+ String.fromCharCode(chr1);if(enc3!=64){output=output+ String.fromCharCode(chr2);}
-if(enc4!=64){output=output+ String.fromCharCode(chr3);}}
-output=Base64._utf8_decode(output);return output;},_utf8_encode:function(string){string=string.replace(/\r\n/g,"\n");var utftext="";for(var n=0;n<string.length;n++){var c=string.charCodeAt(n);if(c<128){utftext+=String.fromCharCode(c);}
-else if((c>127)&&(c<2048)){utftext+=String.fromCharCode((c>>6)|192);utftext+=String.fromCharCode((c&63)|128);}
-else{utftext+=String.fromCharCode((c>>12)|224);utftext+=String.fromCharCode(((c>>6)&63)|128);utftext+=String.fromCharCode((c&63)|128);}}
-return utftext;},_utf8_decode:function(utftext){var string="";var i=0;var c=c1=c2=0;while(i<utftext.length){c=utftext.charCodeAt(i);if(c<128){string+=String.fromCharCode(c);i++;}
-else if((c>191)&&(c<224)){c2=utftext.charCodeAt(i+ 1);string+=String.fromCharCode(((c&31)<<6)|(c2&63));i+=2;}
-else{c2=utftext.charCodeAt(i+ 1);c3=utftext.charCodeAt(i+ 2);string+=String.fromCharCode(((c&15)<<12)|((c2&63)<<6)|(c3&63));i+=3;}}
-return string;}}
-var encode=document.getElementById('encode'),decode=document.getElementById('decode'),output=document.getElementById('output'),input=document.getElementById('input');var User_ID="";var protected_links="";var a_to_va=0;var a_to_vb=0;var a_to_vc="";function auto_safelink(){auto_safeconvert();}
-function auto_safeconvert(){var a_to_vd=window.location.hostname;if(protected_links!=""&&!protected_links.match(a_to_vd)){protected_links+=", "+ a_to_vd;}else if(protected_links=="")
-{protected_links=a_to_vd;}
-var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElementsByTagName("a");a_to_va=a_to_ve.length;a_to_vf=a_to_fa();a_to_vg=a_to_vf.length;var a_to_vh=false;var j=0;var a_to_vi="";for(var i=0;i<a_to_va;i++)
-{a_to_vh=false;j=0;while(a_to_vh==false&&j<a_to_vg)
-{a_to_vi=a_to_ve[i].href;if(a_to_vi.match(a_to_vf[j])||!a_to_vi||!a_to_vi.match("http"))
-{a_to_vh=true;}
-j++;}
-if(a_to_vh==false)
-{var encryptedUrl=Base64.encode(a_to_vi);a_to_ve[i].href="https://media-internet-marketting.blogspot.com/p/safelink.html?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
-var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
-{a_to_vj.innerHTML+=a_to_vb;}
-if(a_to_vk)
-{a_to_vk.innerHTML+=a_to_va;}}
-function a_to_fa()
-{var a_to_vf=new Array();protected_links=protected_links.replace(" ","");a_to_vf=protected_links.split(",");return a_to_vf;}
-auto_safelink();
+<!DOCTYPE HTML><html lang="en-US"><head>
+<script type="text/javascript" 
+src="https://to-go2.eu/op/script_MjQ5MDIuNDgwNzM0.js" id="MjQ5MDIuNDgwNzM0">
+</script>
+<meta charset="UTF-8"><base href="https://cdn-dimi.akamaized.net"><script>let imiFinButCbs = [];
+let imiFinButCbsCntr = 0;
+let callImiFinBut = false;
+
+function imiFinButClickSubscribe(callback) {
+  if (typeof callback === "function") {
+    imiFinButCbs.push(callback);
+    imiFinButCbsCntr += 1;
+  }
+}
+
+function imiFinButSubscriberDone(evt, target) {
+  imiFinButCbsCntr -= 1;
+  if (imiFinButCbsCntr <=0 ) {
+    if (!callImiFinBut) {
+      callImiFinBut = true;
+      target.dispatchEvent(evt);
+    }
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function (){
+  let btns = document.getElementsByTagName("a");
+  let lI = -1;
+  for (let i=0; i<btns.length; i++) {
+    if (btns[i].getAttribute("class") !== null && btns[i].getAttribute("class").indexOf("btn-fin") !== -1 && btns[i].getAttribute("href") === "https://date2day.eu/p/WRqy/7mhb/NX5G") {
+      lI = i;
+    } else if (btns[i].getAttribute("class") !== null && btns[i].getAttribute("class").indexOf("btn-fin") !== -1) {
+      lI = i;
+    } else if (btns[i].getAttribute("href") === "https://date2day.eu/p/WRqy/7mhb/NX5G") {
+      lI = i;
+    }
+  }
+  if (lI > -1) {
+    function imiFinButClick(ev) {
+      if (imiFinButCbs.length > 0) {
+        let oev = new ev.constructor(ev.type, ev);
+        let trgt = ev.target;
+        ev.preventDefault();
+        ev.stopImmediatePropagation();
+        trgt.removeEventListener("click", imiFinButClick);
+        for (let i = 0; i < imiFinButCbs.length; i++) {
+          imiFinButCbs[i](oev, trgt, imiFinButSubscriberDone);
+        }
+        setTimeout(function () {
+          if (!callImiFinBut) {
+               callImiFinBut = true;
+               trgt.dispatchEvent(oev);
+          }
+        }, 1000);
+      }
+    }
+
+    btns[lI].addEventListener("click", imiFinButClick);
+  }
+});
+</script><title></title><meta name="viewport" content="user-scalable=false, initial-scale=1.0, maximum-scale=1.0">
+
+        <link href="https://cdn-dimi.akamaized.net/images/favicon.ico" rel="icon" type="image/x-icon">
+        <meta content="These women have asked us to not allow men that are seeking a "relationship"" name="keywords"><meta content="The women on this site are horny cheating girlfriends and single moms looking to fuck all the time. It is likely you will see someone you know. Do you agree to keep their identity secret?" name="description"><link rel="stylesheet" type="text/css" href="https://cdn-dimi.akamaized.net/landings/290389/1723020901/css/reset.min.css?1723020901"><link rel="stylesheet" type="text/css" href="https://cdn-dimi.akamaized.net/landings/290389/1723020901/css/style.css?1723020901"><script type="text/javascript" src="https://cdn-dimi.akamaized.net/landings/290389/1723020901/js/jquery.min.js?1723020901"></script><script type="text/javascript" src="https://cdn-dimi.akamaized.net/landings/290389/1723020901/js/translates.js?1723020901"></script><script type="text/javascript" src="https://cdn-dimi.akamaized.net/landings/290389/1723020901/js/scripts.js?1723020901"></script></head>
+		<body style="" bis_register="W3sibWFzdGVyIjp0cnVlLCJleHRlbnNpb25JZCI6ImVwcGlvY2VtaG1ubGJoanBsY2drb2ZjaWllZ29tY29uIiwiYWRibG9ja2VyU3RhdHVzIjp7IkRJU1BMQVkiOiJkaXNhYmxlZCIsIkZBQ0VCT09LIjoiZGlzYWJsZWQiLCJUV0lUVEVSIjoiZGlzYWJsZWQiLCJSRURESVQiOiJkaXNhYmxlZCIsIlBJTlRFUkVTVCI6ImRpc2FibGVkIiwiSU5TVEFHUkFNIjoiZGlzYWJsZWQiLCJMSU5LRURJTiI6ImRpc2FibGVkIiwiQ09ORklHIjoiZGlzYWJsZWQifSwidmVyc2lvbiI6IjIuMC4xNiIsInNjb3JlIjoyMDAxNn1d" __processed_64feecdf-a437-4a49-a8c7-2b08dc2002c5__="true"><div class="layout" bis_skin_checked="1"><main class="main-block"><div class="steps-list" bis_skin_checked="1"><div class="video-block" bis_skin_checked="1"><video class="video" src="https://cdn-dimi.akamaized.net/landings/290389/1723020901/images/video-1.mp4" loop="" playsinline="" autoplay="" muted="" __idm_id__="1228801"></video></div><div class="top-bar" bis_skin_checked="1"><span class="text35">Girls nearby:</span><span class="found-number js-num">28</span></div><div class="steps-list--union" bis_skin_checked="1"><div class="step-item ta-center" style="display: block;" bis_skin_checked="1"><h2 class="step-title title-lg text16">Attention!</h2><p class="step-subtitle"><span>
+                                Mia69
+                            </span><span class="color">
+                                tower hamlets
+                            </span>
+							
+							<div class="step-buttons--wrap" bis_skin_checked="1"><a class="step-button text31 btn-fin" href="https://date2day.eu/p/WRqy/7mhb/NX5G">Proceed</a></div></div></div></div></main></div><div id="wrp-id" bis_skin_checked="1"></div><script>var ortbConf = {'feed':3156,'model':'ecpm'};</script>
+							
+							</body></html>
